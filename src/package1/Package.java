c71506faf02a24e9d664367e5fad407071b50364
theengineerscam <theengineerscam@gmail.com>
2025-04-05 20:33:10 +0530

    automated user story

--- src/package1/Package.java
+++ src/package1/Package.java
@@ -1,9 +1,17 @@
 package package1;
 
 public class Package {
+	
+	public void add()
+	{
+		System.out.println(" THis is Method");
+	}
 
 	public static void main(String[] args) {
 		
+		{
+			System.out.println(" The Code is push to github");
+		}
 
 	}
 
